e9226cdd493d431ba7c3b1b5cf287f80eb9fd39d
PGabriel20 <bielporreca@gmail.com>
2023-06-09 15:26:01 -0300

    Added theme provider and extended colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,14 +8,20 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'light-content': '#A7A7A7',
+        'dark-heading': '#1f2428',
+        'dark-content': '#666666',
+        'light-heading': '#CCCCCC',
+        'dark-mode': '#191919',
+        'dark-card': '#363636',
+        'green-text': '#018C0F',
+        greenbg: '#D7FFE0',
       },
     },
     fontFamily: {
-      poppins: ['Poppins', 'sans-serif'],
-      'dm-sans': ['DM Sans', 'sans-serif'],
+      primary: ['Poppins', 'sans-serif'],
+      secondary: ['DM Sans', 'sans-serif'],
     },
   },
   plugins: [],
