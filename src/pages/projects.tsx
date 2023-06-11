2c0b085f3dd317742371672e291cb165d8000bd1
PGabriel20 <bielporreca@gmail.com>
2023-06-11 13:28:47 -0300

    Adjusted leadings

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -6,11 +6,11 @@ const Projects: React.FC = () => {
       <h1 className='text-2xl text-dark-heading dark:text-light-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold'>
         My Portifolio
       </h1>
-      <p className='md:mt-4 text-lg leading-9 md:leading-7'>
+      <p className='md:mt-4 py-6 text-lg leading-6 md:leading-7'>
         Here are some of my personal/open source projects. Feel free contribute to these projects by
         making pull requests. Your contributions are highly appreciated.
       </p>
-      <section className='grid grid-cols-1 md:grid-cols-2 gap-x-10 mt-8'>
+      <section className='grid grid-cols-1 md:grid-cols-2 gap-x-10 mt-2'>
         <ProjectCard
           title='This'
           description='My personal portifolio'
